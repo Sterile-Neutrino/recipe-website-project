d7a61ad9ed6e71cec036011b43125cc633b01559
tracyZhang9999 <tracyzhang0919@g.ucla.edu>
2021-11-09 21:16:08 -0800

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,52 +1,58 @@
 import React, {useState} from 'react';
-import { Route, Router } from 'react-router';
+import axios from "axios";
+import "./App.css"
+
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+//import components here
+import SignUp from "./components/SignUpForm";
 import LoginForm from './components/LoginForm';
-//import SignUpForm from './components/SignUpForm';
+
+axios.defaults.withCredentials = true;
+
 
 function App() {
-  //default email formula
-  const adminUser={
-    name: "username",
-    password: "123456"
-  }
-
-//useState hook to initilize state
-  const [user,setUser] = useState({name: ""});
-  const [error, setError] = useState("");
-
-  const Login = details => {
-    //console.log() method writes a message to the console.
-    console.log(details);
-    //if username matches  password
-    if(details.name==adminUser.name && details.password==adminUser.password){
-      console.log("Logged in")
-      setUser({
-        name: details.name
-      });
-    }else{
-      console.log("Username password combination not match")
-      setError("Username password combination not match")
-    }
-
-  }
-
-  const Logout =() => {
-    setUser({name:""});
-  }
-
-  return (
-    <div className="App">
-      {(user.name !="") ? (
-        <div className="welcome">
-          <h2>Welcome, <span>{user.name}</span></h2>
-          <button onClick={Logout}>Logout</button>
-        </div>
-      ):(
-        <LoginForm Login={Login} error={error}/>
+  return(
+    <Router>
+      <div className="App">
         
-      )}
-    </div>
+      
+        <div className="top-bar"></div>
+        <nav>
+          <Link to="/" className="logo"><h1>My Recipe App</h1></Link>
+          <Link to="/" className="Home">
+            My Account
+          </Link>
+          <Link to="/LoginForm" className="LoginForm">
+            Login
+          </Link>
+          <Link to="/SignUp" className="Signup">
+            Register
+          </Link>
+        </nav>
+       
+         <Routes>
+          <Route exact path="/SignUp" exact element={<SignUp/>}/>
+          <Route exact path="/LoginForm" exact element={<LoginForm/>}/>
+
+
+        </Routes>
+
+
+      </div>
+
+    </Router>
+
   );
 }
+
+
+
+
+
+
+
+
+
 
 export default App;
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,39 +1,140 @@
 import React, {useState} from 'react';
+import SignUp from './SignUpForm';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function LoginForm({Login, error}) {
-    const [details, setDetails]=useState({name:"",  password:""});
 
-    const submitHandler = e => {
-        e.preventDefault();
 
-        Login(details);
+class LoginForm extends React.Component{
+
+    constructor(props){
+        super(props);
+        /// Setting up state
+        this.state={
+            username: "",
+            password: "",
+            IsCompleted: false,
+            redirect: null,
+            LoggedIn: false
+
+        };
+        /// Setting up functions - set 'this' context to this class
+        this.inputUsername=this.inputUsername.bind(this);
+        this.inputPassword=this.inputPassword.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
     }
-    return (
-        <form onSubmit={submitHandler}>
+
+    //input functions
+    inputUsername(event){
+        this.setState({
+            username: event.target.value
+        });
+    }
+    inputPassword(event){
+        this.setState({
+            password: event.target.value
+        });
+    }
+
+    finishLogin(){
+        this.setState({
+            IsCompleted: true
+        });
+    }
+    
+ /*
+      CheckLoggedIn({
+        axios
+        .post("Some address http://localhost")
+        .then((res) => {
+          // if true, means logged in
+          this.setState({ loggedIn: true, redirect: "/" });
+        })
+        .catch((err) => {
+           // if err, means not logged in, so valid for logging
+          this.setState({ loggedIn: false });
+        });
+
+      })
+      */
+
+
+
+      //submit the data
+    onSubmit(event){
+        event.preventDefault();
+        const LoginData={
+            username: this.state.username,
+            password: this.state.password
+        };
+        if(LoginData.username===""|| LoginData.password==="" ){
+            alert("Cannot be empty form!");
+            return;
+        }
+        /*axios
+      .post("some address", objObject)
+      .then((res) => {
+        // only remove if complete successfully
+        this.setState({ username: "", password: "" });
+        this.setState({ redirect: "/Home" });
+      })
+      .catch((err) => {
+        // if error, notify user
+        this.setState({ name: "", password: "" });
+        alert(err.response.data.message);
+      });*/
+
+
+
+
+
+    }
+    render(){
+        if(this.state.redirect){
+            return <Link to={this.state.redirect}/>
+        }
+        if(!this.IsCompleted){
+            return(
+        <div className="SignInBox" >
+        <form className="input_form" onSubmit={this.onSubmit}>
             <div className="form-inner">
-                <h2>Login</h2>
-                {(error !="") ? (<div className="error">{error}</div>): ""}
+                <h2>Log In</h2>
+                
+               
                 <div className="form-group">
                     <label htmlFor="name">UserName:</label>
-                    <input type="text" name="name" id="name" onChange={e =>setDetails({...details, name:e.target.value})} value={details.name} />
+                    <input type="text" placeholder="Sam Lee" name="name" id="name" onChange={this.inputUsername} value={this.state.username} />
                 </div>
+
                 <div className="form-group">
                     <label htmlFor="password">Password:</label>
-                    <input type="password" name="password" id="password" onChange={e =>setDetails({...details, password:e.target.value})} value={details.password} />
+                    <input type="password" placeholder="123456" name="password" id="password" onChange={this.inputPassword} value={this.state.password} />
                 </div>
-                <input type="submit" value="LOGIN"/>
-                <p class="form__text">
-                    <a class="form__link" href="./" id="linkCreateAccount">Don't have an account? Create account!</a>
-                </p>
+
+                <input type="submit" value="LOGIN" onClick={()=> this.finishLogin()}/>
+                
                 
             </div>
             
         </form>
-        
+        </div>
+
+            )
+        }
+        else{
+            return(
+                <LoginForm name={this.state.username}
+                    password={this.state.password}/>
+            );
+        }
+    
+    }
 
 
-        
-    )
+
 }
+
+
+
 
 export default LoginForm
