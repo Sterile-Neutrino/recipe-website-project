6069250af4d045ee9645b9440db3482597fdf432
Ruichen Zheng <ruichen.zheng118@gmail.com>
2021-11-29 16:14:33 -0800

    Restore changes in recipePage.js

--- client/src/components/RecipeList.js
+++ client/src/components/RecipeList.js
@@ -1,34 +1,126 @@
-import React from "react";
-import sample from '../background.jpg'
-import PostRecipe from './PostRecipe.js';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./recipePage.css"
 
+function RecipeName (name){
+    return (
+        <div className="recipeNameBlock">
+            <h1 className="recipe_name">
+                {name}
+            </h1>
+        </div>
+    );
+}
+function RecipeDescription(description) {
 
+    return (
+      <div className="descriptionBlock">
+        <h1 className="description_content">
+        {description}
+        </h1>
+      </div>
+    );
+}
+function RecipeCalories(calories) {
 
-export default class RecipeList extends React.Component {
-    renderPosts(arr) {
-        const postArr = arr.map((post, index) => {
-            return (
-                <PostRecipe
-                    key={post.id}
-                    image={post.imageId === undefined ? sample : `http://localhost:4000/recipes/upload/${post.imageId}`}
-                    title={post.title}
-                    author={post.author}
-                    description={post.description}
-                    ingredient={post.ingredient}
-                    like={post.like}
-                    category={post.category}
-                    handleLike={() => this.props.handleLike(index)}
-                />
-            )
-        });
-        return <ul>{postArr}</ul>;
+    return (
+      <div className="caloriesBlock">
+        <h1 className="recipe_calories">
+        {calories}
+        </h1>
+      </div>
+    );
+}
+function RecipePicture() {
+
+    return (
+      <div className="picture">
+        <img src="/recipes/display/kung-pao-chicken.jpeg" alt=""/>
+      </div>
+    );
+}
+
+function Like(){
+  return(
+    <div className="LikeButton">
+      <button onClick={likeit}>
+        Like this recipe 👍🏻
+      </button>
+    </div>
+  );
+}
+function likeit(){
+  ;
+}
+
+function Favorite(){
+  return(
+    <div className="FavoriteButton">
+      <button onClick={AddtoFavorite}>
+        Add to my list ⭐
+      </button>
+    </div>
+  );
+}
+
+function AddtoFavorite(){
+  ;
+}
+
+class recipePage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+          title:'',//gets a list of recipes
+          description:''
+        };
+    }
+
+    componentDidMount = ()=>{
+      this.getRecipe();
+    }
+
+    getRecipe=()=>{
+      var self=this;
+      axios.get(`http://localhost:4000/recipes/61a4a21083e49fbd03fefcb8`)
+        .then((response)=>{
+          self.setState({title:response.data.title})
+          self.setState({description:response.data.description})
+          self.setState({calories:response.data.calories})
+          console.log(response.data);
+        })
     }
 
     render() {
-        return (
-            <div className="post-list">
-                {this.renderPosts(this.props.postArr)}
-            </div>
-        )
+      return (
+        
+        <div>
+          <div className="RecipeName"> 
+            {RecipeName(this.state.title)}
+          </div>
+          <div className="RecipeDescription">
+            {RecipeDescription(this.state.description)}
+          </div>
+          <div className="RecipeCalories">
+           {RecipeCalories(this.state.calories)}
+          </div>
+          <div className="RecipePicture">
+            <RecipePicture/>
+          </div>
+          <div className="Like">
+            <Like/>
+          </div>
+          <div className="Favorite">
+            <Favorite/>
+          </div>
+        </div>
+      );
     }
+
 }
+/*
+
+*/
+
+export default recipePage;
