29a4058fc155161b51411f315780ef05064debe1
Ruichen Zheng <ruichen.zheng118@gmail.com>
2021-11-30 16:29:59 -0800

    Now Recipe List in Home Page can dynamically route to each recipe page. Also, search page is functional now. It will display in the same format as other lists, which will be modified later

--- client/src/components/searchResult.js
+++ client/src/components/searchResult.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import "./searchResult.css";
 import range from "lodash/range";
 import { useParams, useLocation } from 'react-router-dom';
+import recipePage from './recipePage';
 
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Upload from './Upload';
@@ -13,33 +14,180 @@ import Upload from './Upload';
 //     console.log(resultList)
 // }
 
-class SearchResult extends React.Component {
+
+// function RecipeItem (recipeID) {
+    
+//     recipeID = recipeID.recipeID
+
+//     var title = ''
+//     axios.get(`http://localhost:4000/recipes/${recipeID}`)
+//     .then((response)=>{
+//         title = response.data.title
+//         console.log(title)
+//     })
+    
+//     return(
+//         <Link to={{
+//             pathname: `/RecipePage/${recipeID}`, //test for dynamic route: path id
+//           }} component={recipePage} className="RecipeItem"  style = {{textDecoration: 'none' }}>
+//         <h1 className="RecipeTitle"> 
+//           {title}
+//         </h1>
+//       </Link>
+//     )
+  
+// };
+
+//   function ResultList(searchResult) {
+//     // const [render, setRender] = useState(true);
+//     const resultList = searchResult.searchResult;
+//     console.log(resultList);
+//     console.log(typeof(resultList));
+
+//     const listR = resultList.map(
+//         (id) => (<RecipeItem recipeID = {id}/>)
+//     )
+
+//     return (
+//       <div>
+//         <h1 className="Title">
+//           Search result!
+//         </h1>
+//             <div className="BasicList">
+//                 {/* {range(resultList.length).map((resultList[]))} */}
+//                 {listR}
+//             </div>
+//           </div>
+//     );
+//   }
+
+// class SearchResult extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.searchWord = '';
+//         this.searchResult = [];
+//     }
+
+//     componentDidMount = ()=>{
+//         this.searchWord = this.props.params.s;
+//           axios.get(`http://localhost:4000/recipes/search/${this.searchWord}`)
+//         .then((response)=>{
+//           this.SearchResult = response.data;
+//           console.log(this.SearchResult); //for debugging
+
+//           for (var value in this.searchResult) {
+//               console.log(value)
+//           }
+//         })
+//         // console.log(this.SearchResult)
+//     }
+
+
+
+//     render() {
+//         return (
+//             <div>
+//                 <strong>
+//                     {this.searchWord}
+//                 </strong>
+//                 <div>
+//                     <ResultList searchResult = {list}/>
+//                 </div>
+//             </div>
+
+
+//         )
+//     }
+// }
+    
+
+function RecipeItem (item) {
+    
+    const recipeID = item.item;
+    const recipeTitle = item.title;
+  
+    
+    // console.log(item)
+    
+    return(
+        <Link to={{
+            pathname: `/RecipePage/${recipeID}`, //test for dynamic route: path id
+          }} component={recipePage} className="RecipeItem"  style = {{textDecoration: 'none' }}>
+        <h1 className="RecipeTitle"> 
+          {recipeTitle}
+        </h1>
+      </Link>
+  
+    )
+  };
+  
+  class SearchResult extends React.Component {
+  
     constructor(props) {
-        super(props)
-        this.searchWord = '';
-        this.searchResult = '';
-    }
-
-    componentDidMount = ()=>{
-        console.log(this.props.params.s)
+      super(props);
+      this.state = {
+          // listID: [],
+          listTitle: [],
+          resultList: []
+          // listItem: {id: [], title: []}
+  
+      };
+      this.listItem = new Object();
+      this.titles = [];
+      this.componentDidMount = this.componentDidMount.bind(this)
+  }
+    componentDidMount () {
         this.searchWord = this.props.params.s;
-          axios.get(`http://localhost:4000/recipes/search/${this.searchWord}`)
+        axios.get(`http://localhost:4000/recipes/search/${this.searchWord}`)
         .then((response)=>{
-          this.SearchResult = response.data;
-          // console.log(response.data); //for debugging
+        //   this.SearchResult = response.data;
+          this.setState({resultList: response.data})
+        //   console.log(this.SearchResult); //for debugging
+
+          for (var value of this.state.resultList) {
+              console.log(value)
+          }
         })
-        console.log(this.SearchResult)
+        .then((response) => {
+            this.getTitle();
+        })
+
+        this.getTitle()
     }
 
+    getTitle = () => {
+        console.log(this.state.resultList)
+        for (var ID of this.state.resultList) {
+            axios.get(`http://localhost:4000/recipes/${ID}`)
+            .then((response)=>{
+                console.log(ID);
+                this.listItem[ID] = response.data.title;
+                this.titles.push(response.data.title)
+                this.setState({listTitle: this.titles})
+                console.log(this.state.listTitle);
+            })
+          }
+    }
+  
     render() {
         return (
-            <strong>
-                {this.searchWord}
-            </strong>
-        )
-    }
-}
-    
+          <div>
+            <h1 className="Title">
+              Search Results!
+            </h1>
+                <div className="BasicList">
+                  {this.state.resultList.map(
+                    (id) => (<RecipeItem item = {id} title = {this.state.listTitle[this.state.resultList.indexOf(id)]}/>)
+                  )}
+                </div>
+              </div>
+        );
+      }
+  
+  }
+  
+  
+
 const withRouter = WrappedComponent => props => {
     const params = useParams();
     // etc... other react-router-dom v6 hooks
